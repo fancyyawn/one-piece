70f4ebc22a97a5b94584f65d650a953e6fa7b3a5
zhanghecheng <hechengopen@gmail.com>
2017-04-27 17:24:22 +0800

    add logout with angular csrf cookie

--- ms-common/ms-passport/src/main/java/top/zhacker/ms/WebSecurityConfig.java
+++ ms-common/ms-passport/src/main/java/top/zhacker/ms/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * DATE: 2017/4/27 <br>
@@ -18,6 +19,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**").authorizeRequests()
                 .antMatchers("/", "/login").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
