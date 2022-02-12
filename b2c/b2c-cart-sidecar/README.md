445fd54acbd43e4bc3f5a1995663ac7a3ca50328
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-12 10:32:40 +0800

    Delete b2c directory

--- b2c/b2c-cart-sidecar/README.md
+++
@@ -1,1 +0,0 @@
-http://www.tuicool.com/articles/BjIFfqf
--- b2c/b2c-cart/src/main/java/top/zhacker/ms/b2c/cart/model/LineItem.java
+++
@@ -1,9 +0,0 @@
-package top.zhacker.ms.b2c.cart.model;
-
-/**
- * DATE: 2017/4/24 <br>
- * MAIL: hechengopen@gmail.com <br>
- * AUTHOR: zhacker
- */
-public class LineItem {
-}
